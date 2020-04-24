10e321d0a3ac37f7d350bc183b6bced0e6bd4061
Michael Larson <mlarson!@mail.csuchico.edu>
2020-04-23 17:12:08 -0700

    Pal Tests 3

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -52,27 +52,46 @@ TEST(PracticeTest, mix_case_palindrome)
 TEST(PracticeTest, space_palindrome)
 {
     Practice obj;
-    bool actual = obj.isPalindrome("A a");
+    bool actual = obj.isPalindrome("bA ab");
     ASSERT_TRUE(actual);
 }
 
 TEST(PracticeTest, order_correct)
-{
-    sortDescending(1, 2, 3);
+{	
+	int first = 1;
+	int second = 2;
+	int third = 3;
+    sortDescending(first, second, third);
     ASSERT_LE(first, second);
 	ASSERT_LE(second, third);
 }
 
 TEST(PracticeTest, order_reverse)
 {
-    sortDescending(3, 2, 1);
+	int first = 3;
+	int second = 2;
+	int third = 1;
+    sortDescending(first, second, third);
     ASSERT_LE(first, second);
 	ASSERT_LE(second, third);
 }
 
 TEST(PracticeTest, order_neg_value)
 {
-    sortDescending(-1, 2, 3);
+	int first = -1;
+	int second = 2;
+	int third = 3;    
+    sortDescending(first, second, third);
+    ASSERT_LE(first, second);
+	ASSERT_LE(second, third);
+}
+
+TEST(PracticeTest, order_same)
+{
+	int first = 44;
+	int second = 44;
+	int third = 44;    
+    sortDescending(first, second, third);
     ASSERT_LE(first, second);
 	ASSERT_LE(second, third);
 }
