c4515ab3d1aa55cbbdd0d684cfb512d6296a915b
Michael Larson <mlarson!@mail.csuchico.edu>
2020-04-23 17:18:53 -0700

    Pal Tests 5

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -56,42 +56,46 @@ TEST(PracticeTest, space_palindrome)
     ASSERT_TRUE(actual);
 }
 
-// TEST(PracticeTest, order_correct)
-// {	
-// 	int first = 1;
-// 	int second = 2;
-// 	int third = 3;
-//     sortDescending(first, second, third);
-//     ASSERT_LE(first, second);
-// 	ASSERT_LE(second, third);
-// }
+TEST(PracticeTest, order_correct)
+{	
+	Practice obj;
+	int first = 1;
+	int second = 2;
+	int third = 3;
+    obj.sortDescending(first, second, third);
+    ASSERT_LE(first, second);
+	ASSERT_LE(second, third);
+}
 
-// TEST(PracticeTest, order_reverse)
-// {
-// 	int first = 3;
-// 	int second = 2;
-// 	int third = 1;
-//     sortDescending(first, second, third);
-//     ASSERT_LE(first, second);
-// 	ASSERT_LE(second, third);
-// }
+TEST(PracticeTest, order_reverse)
+{
+	Practice obj;
+	int first = 3;
+	int second = 2;
+	int third = 1;
+    obj.sortDescending(first, second, third);
+    ASSERT_LE(first, second);
+	ASSERT_LE(second, third);
+}
 
-// TEST(PracticeTest, order_neg_value)
-// {
-// 	int first = -1;
-// 	int second = 2;
-// 	int third = 3;    
-//     sortDescending(first, second, third);
-//     ASSERT_LE(first, second);
-// 	ASSERT_LE(second, third);
-// }
+TEST(PracticeTest, order_neg_value)
+{
+	Practice obj;
+	int first = -1;
+	int second = 2;
+	int third = 3;    
+    obj.sortDescending(first, second, third);
+    ASSERT_LE(first, second);
+	ASSERT_LE(second, third);
+}
 
-// TEST(PracticeTest, order_same)
-// {
-// 	int first = 44;
-// 	int second = 44;
-// 	int third = 44;    
-//     sortDescending(first, second, third);
-//     ASSERT_LE(first, second);
-// 	ASSERT_LE(second, third);
-// }
+TEST(PracticeTest, order_same)
+{
+	Practice obj;
+	int first = 44;
+	int second = 44;
+	int third = 44;    
+    obj.sortDescending(first, second, third);
+    ASSERT_LE(first, second);
+	ASSERT_LE(second, third);
+}
